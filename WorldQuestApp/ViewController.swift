09776de2b75701b36b8f7da621ed979950b22064
Márcio Santos do Carmo <marcio.carmo@172-16-2-149.0000.epoa.br.iosda.org>
2018-05-11 11:39:26 -0300

    teste

--- WorldQuestApp/ViewController.swift
+++ WorldQuestApp/ViewController.swift
@@ -1,25 +1,9 @@
-//
-//  ViewController.swift
-//  WorldQuestApp
-//
-//  Created by Alan Quadros on 11/05/2018.
-//  Copyright © 2018 World Quest. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
