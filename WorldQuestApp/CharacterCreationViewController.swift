5684f209bb9e1c111330ec9cba5a8974b796d8d9
Leonardo Marcelino Vieira <leonardo.vieira@172-16-1-40.0000.epoa.br.iosda.org>
2018-05-14 13:26:54 -0300

    Storyboard developement, falta constraints de atributos

--- WorldQuestApp/CharacterCreationViewController.swift
+++ WorldQuestApp/CharacterCreationViewController.swift
@@ -8,100 +8,54 @@
 
 import UIKit
 
-class CharacterCreationViewController: UITableViewController {
+class CharacterCreationViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
     //uicollectionview carousel swift
+        
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        tableView.delegate = self
-        tableView.dataSource = self
-        
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
+    
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
+    func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 5
+        
     }
 
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "CreateButtonCell", for: indexPath)
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell
+        
         switch(indexPath.row) {
         case 0:
-            cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
+            cell = self.tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
             break
         case 1:
-            cell = tableView.dequeueReusableCell(withIdentifier: "ClassCell", for: indexPath)
+            cell = self.tableView.dequeueReusableCell(withIdentifier: "ClassCell", for: indexPath)
             break
         case 2:
-            cell = tableView.dequeueReusableCell(withIdentifier: "ClassAbilitiesCell", for: indexPath)
+            cell = self.tableView.dequeueReusableCell(withIdentifier: "ClassAbilitiesCell", for: indexPath)
             break
         case 3:
-            cell = tableView.dequeueReusableCell(withIdentifier: "AttributesCell", for: indexPath)
+            cell = self.tableView.dequeueReusableCell(withIdentifier: "AttributesCell", for: indexPath) as! CharacterCreationAttributesTableViewCell
             break
         default:
-            cell = tableView.dequeueReusableCell(withIdentifier: "CreateButtonCell", for: indexPath)
+            cell = self.tableView.dequeueReusableCell(withIdentifier: "CreateButtonCell", for: indexPath)
             break
         }
+        
 
         return cell
     }
- 
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
+    
     /*
     // MARK: - Navigation
 
