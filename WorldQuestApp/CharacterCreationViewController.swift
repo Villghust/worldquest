477014d0159283a69f25f38ee3a6e7a3a8ef35b4
Leonardo Marcelino Vieira <leonardo.vieira@172-16-1-40.0000.epoa.br.iosda.org>
2018-05-14 10:50:17 -0300

    Ä few alterations

---
+++ WorldQuestApp/CharacterCreationViewController.swift
@@ -0,0 +1,115 @@
+//
+//  CharacterCreationViewController.swift
+//  WorldQuestApp
+//
+//  Created by Leonardo Marcelino Vieira  on 14/05/2018.
+//  Copyright © 2018 World Quest. All rights reserved.
+//
+
+import UIKit
+
+class CharacterCreationViewController: UITableViewController {
+    
+    //uicollectionview carousel swift
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 5
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "CreateButtonCell", for: indexPath)
+        switch(indexPath.row) {
+        case 0:
+            cell = tableView.dequeueReusableCell(withIdentifier: "NameCell", for: indexPath)
+            break
+        case 1:
+            cell = tableView.dequeueReusableCell(withIdentifier: "ClassCell", for: indexPath)
+            break
+        case 2:
+            cell = tableView.dequeueReusableCell(withIdentifier: "ClassAbilitiesCell", for: indexPath)
+            break
+        case 3:
+            cell = tableView.dequeueReusableCell(withIdentifier: "AttributesCell", for: indexPath)
+            break
+        default:
+            cell = tableView.dequeueReusableCell(withIdentifier: "CreateButtonCell", for: indexPath)
+            break
+        }
+
+        return cell
+    }
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
