aef4c91c2884d0f1d724092a83b2af6258d3c331
Guilherme Piccoli <guilherme.piccoli@172-16-2-4.0000.epoa.br.iosda.org>
2018-05-15 12:16:11 -0300

    Implementacao básica dos componentes na tela, dados mockados

---
+++ WorldQuestApp/GroupViewController.swift
@@ -0,0 +1,47 @@
+//
+//  GroupViewController.swift
+//  WorldQuestApp
+//
+//  Created by Guilherme Piccoli on 15/05/2018.
+//  Copyright © 2018 World Quest. All rights reserved.
+//
+
+import UIKit
+
+class GroupViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+   // @IBOutlet weak var imgLeader: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell : UITableViewCell
+        
+        switch indexPath.row {
+        case 0:
+            cell = self.tableView.dequeueReusableCell(withIdentifier: "Party Leader", for: indexPath) //as! PartyLeaderCell
+            break
+            
+        default:
+            cell = self.tableView.dequeueReusableCell(withIdentifier: "Party Member", for: indexPath)
+            break
+        }
+        
+        return cell
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+}
---
+++ WorldQuestApp/PartyLeaderCell.swift
@@ -0,0 +1,28 @@
+//
+//  PartyLeaderCellTableViewCell.swift
+//  WorldQuestApp
+//
+//  Created by Guilherme Piccoli on 15/05/2018.
+//  Copyright © 2018 World Quest. All rights reserved.
+//
+
+import UIKit
+
+class PartyLeaderCell: UITableViewCell {
+
+ 
+    @IBOutlet weak var imgLeader: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var characaterLabel: UILabel!
+    @IBOutlet weak var imgCharacter: UIImageView!
+    
+//    var partyLeader {
+//        didSet {
+//            self.imgLeader.image = UIImage.init(named:"party_leader")
+//            self.nameLabel.text = "Sir Evangivaldo"
+//            self.characaterLabel.text = "Guerreiro Nível 4"
+//            self.imgCharacter.image = UIImage.init(named: "_CHAR_MAGE")
+//        }
+//    }
+
+}
